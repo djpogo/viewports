6647c059605e78673de80ad49d78cabbb7176eaf
Raoul <git@raoulkramer.de>
2024-10-29 17:58:42 +0100

    add prefers-* media queries

--- main.js
+++ main.js
@@ -11,6 +11,11 @@ const pointerNone = window.matchMedia('(pointer: none)').matches;
 const hoverNone = window.matchMedia('(hover: none)').matches;
 const hoverHover = window.matchMedia('(hover: hover)').matches;
 
+const prefersDarkColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+const prefersHighContrast = window.matchMedia('(prefers-contrast: high)').matches;
+const prefersReducedTransparency = window.matchMedia('(prefers-reduced-transparency: reduce)').matches;
+
 const buildContent = () => `
   <ul class="ul">
     <li>Viewport: <strong>${window.innerWidth}px&nbsp;&times;&nbsp;${window.innerHeight}px</strong></li>
@@ -25,8 +30,13 @@ const buildContent = () => `
     <li>100dvw&nbsp;&times;&nbsp;100dvh: ${window.getComputedStyle(dynamicViewport).width}&nbsp;&times;&nbsp;${window.getComputedStyle(dynamicViewport).height}</li>
     <li>100lvw&nbsp;&times;&nbsp;100lvh: ${window.getComputedStyle(largeViewport).width}&nbsp;&times;&nbsp;${window.getComputedStyle(largeViewport).height}</li>
     <li class="spaced">&nbsp;</li>
-    <li>Pointer: <strong>Fine: ${pointerFine ? '✓' : '✗'}</strong> / <strong>Coarse: ${pointerCoarse ? '✓' : '✗'}</strong> / <strong>None: ${pointerNone ? '✓' : '✗'}</strong></li>
-    <li>Hover: <strong>Hover: ${hoverHover ? '✓' : '✗'}</strong> / <strong>None ${hoverNone ? '✓' : '✗'}</strong></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/pointer" target="_blank" rel="noopener noreferrer">Pointer</a>: <strong>Fine: ${pointerFine ? '✓' : '✗'}</strong> / <strong>Coarse: ${pointerCoarse ? '✓' : '✗'}</strong> / <strong>None: ${pointerNone ? '✓' : '✗'}</strong></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/hover" target="_blank" rel="noopener noreferrer">Hover</a>: <strong>Hover: ${hoverHover ? '✓' : '✗'}</strong> / <strong>None ${hoverNone ? '✓' : '✗'}</strong></li>
+    <li class="spaced">&nbsp;</li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme" target="_blank" rel="noopener noreferrer">Prefers Color Scheme</a>: <strong>light ${!prefersDarkColorScheme ? '✓' : '✗'} / dark ${prefersDarkColorScheme ? '✓' : '✗'}</strong></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-contrast" target="_blank" rel="noopener noreferrer">Prefers High Contrast</a>: <strong>${prefersHighContrast ? '✓' : '✗'}</strong></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion" target="_blank" rel="noopener noreferrer">Prefers Reduced Motion</a>: <strong>${prefersReducedMotion ? '✓' : '✗'}</strong></li>
+    <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-transparency" target="_blank" rel="noopener noreferrer">Prefers Reduced Transparency</a>: <strong>${prefersReducedTransparency ? '✓' : '✗'}</strong></li>
   </ul>
 `;
 
